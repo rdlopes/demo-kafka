78460eeb35e5f17520fba75290ab2dfe34a535de
Rui Lopes <rui.d.lopes@me.com>
2026-04-16 11:34:14 +0200

    chore: add living documentation

--- src/test/java/io/github/rdlopes/kafka/CucumberTest.java
+++ src/test/java/io/github/rdlopes/kafka/CucumberTest.java
@@ -5,9 +5,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "io.github.rdlopes.kafka")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber/report.json")
 public class CucumberTest {
 }
